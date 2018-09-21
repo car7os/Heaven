49a832de3d2dab71005759052c77504536f67fc5
root <carlosbezerravilela@gmail.com>
2018-09-20 22:06:13 -0300

    Estrutura do Projeto Estrutura Padrão

---
+++ app/src/main/java/projeto/heaven/Heaven.java
@@ -0,0 +1,13 @@
+package projeto.heaven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Heaven extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heaven);
+    }
+}
