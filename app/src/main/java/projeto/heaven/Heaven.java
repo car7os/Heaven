9f9905f71b0fcfb30b138665e1838790160a3706
root <carlosbezerravilela@gmail.com>
2018-09-21 21:07:28 -0300

    Projeto Classe Principal: Funcionalidades de Reconhecimento de Voz

--- app/src/main/java/projeto/heaven/Heaven.java
+++ app/src/main/java/projeto/heaven/Heaven.java
@@ -1,13 +1,64 @@
 package projeto.heaven;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Heaven extends AppCompatActivity {
+
+    private static final int codigo = 100;
+    private TextView txtSaida;
+    private ImageButton btnAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heaven);
+
+        txtSaida = (TextView) findViewById(R.id.transcreverTexto);
+        btnAudio = (ImageButton) findViewById(R.id.btnFalar);
+
+        btnAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entradaAudio();
+            }
+        });
+    }
+
+    private void entradaAudio() {
+     /** https://stacktips.com/tutorials/android/speech-to-text-in-android Visualizado em 21/09/2018**/
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, codigo);
+        } catch (ActivityNotFoundException a) {
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case codigo: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSaida.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
